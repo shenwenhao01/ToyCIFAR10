485b2467d25ad41edd8f34491b6f41755eac33ef
shenwenhao01 <shenwenhao01@gmail.com>
2021-12-17 14:40:20 +0800

    fix vis

--- vis.py
+++ vis.py
@@ -18,20 +18,33 @@ def load_log(file):
 def main():
     plt.rcParams['figure.figsize'] = (12, 8)
     shows = {}
-    shows['vgg11'] = load_log('outputs/logs/default_vgg11.out')
-    print(shows)
-    #shows['vgg11_bn'] = load_log('log_ha_vgg11')
+    #shows['vgg11'] = load_log('outputs/logs/default_vgg11.out')
+    #shows['vgg13'] = load_log('outputs/logs/resume_vgg13.out')
+    #shows['vgg13_0.5_norm'] = load_log('outputs/logs/vgg13_0.5_norm.out')
+    #shows['vgg13_w/o_norm'] = load_log('outputs/logs/vgg13_wo_norm.out')
+    #shows['vgg13_mix_norm'] = load_log('outputs/logs/vgg13_nores_norm.out')
+    #shows['vgg13_bn'] = load_log('outputs/logs/default_vgg13_bn.out')
+    #shows['random_noise_vgg13_bn'] = load_log('outputs/logs/randnoise_vgg13_bn.out')
+    #shows['sp_noise_vgg13_bn'] = load_log('outputs/logs/spnoise_vgg13_bn.out')
+    #shows['gauss_noise_vgg13_bn'] = load_log('outputs/logs/gaussnoise_vgg13_bn.out')
+    #shows['vgg16'] = load_log('outputs/logs/default_vgg16.out')
+    #shows['vgg19'] = load_log('outputs/logs/default_vgg19.out')
+    shows['mobilenetv2_lr=0.05'] = load_log('outputs/logs/default_mobilenetv2.out')
+    shows['mobilenetv2_lr=0.1'] = load_log('outputs/logs/lr_mobilenetv2.out')
+    shows['mobilenetv2_lr=0.01'] = load_log('outputs/logs/mobilenetv2_lr_0.01.out')
+    #shows['vgg13_bn'] = load_log('outputs/logs/default_vgg13_bn.out')
+    #shows['vgg16_bn'] = load_log('outputs/logs/default_vgg16_bn.out')
+    #shows['vgg19_bn'] = load_log('outputs/logs/default_vgg19_bn.out')
 
-    #shows['vgg13'] = load_log('log_vgg13')
-    #shows['vgg13_half'] = load_log('log_half_vgg13')
-    #shows['vgg13_bn'] = load_log('log_vgg13_bn')
-    #shows['vgg13_half_bn'] = load_log('log_half_vgg13_bn')
     for key in sorted(shows.keys()):
         print(key)
+        for i in range(50):
+            if (i+1)%20 == 0:
+                print(format(shows[key][i],'.2f'))
         epochs = np.arange(1, 1+len(shows[key]))
-        plt.plot(epochs, shows[key], label='{}:{}'.format(key, np.max(shows[key])))
+        plt.plot(epochs, shows[key], label='{:16}{}'.format(key, np.max(shows[key])))
     #plt.legend(shows.keys(), loc='upper left')
-    plt.legend(bbox_to_anchor=(1.05, 1), loc='best', borderaxespad=0.)
+    plt.legend(bbox_to_anchor=(0.8, 0.8), loc=0, borderaxespad=0.)
     plt.show()
     plt.savefig('outputs.png')
 
